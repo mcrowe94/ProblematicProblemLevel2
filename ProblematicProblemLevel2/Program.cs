880fcd5964b3e2fe6d41cacaacea235fba2a102a
Cruz Sanchez <cruzsanchez9209@gmail.com>
2022-08-01 16:51:48 -0500

    Add project files.

---
+++ ProblematicProblemLevel2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProblematicProblemLevel2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
